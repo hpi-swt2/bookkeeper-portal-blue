c08d2d41e4b46d45d093748ddc73f6bf5709b647
Greenscreen23 <43916057+Greenscreen23@users.noreply.github.com>
2022-11-16 20:08:39 +0100

    Rename users/registration_controller to users_controller This was requests by @Sohn123, as it did not make sense for the profile page to be managed by a registration controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # https://github.com/heartcombo/devise/blob/main/README.md
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { registrations: 'users' }
   devise_scope :user do
-    get 'profile', to: 'users/registrations#profile'
+    get 'profile', to: 'users#profile'
   end
 
   # Defines the root path route ("/")
