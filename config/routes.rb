0156fb1eeee3319da0cb8dbb2f25bfe19d9da9ab
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2022-11-17 09:03:16 +0100

    Create all blank pages (#63)
    * Add dashboard
    
    Co-authored-by: Lukas Hagen <Greenscreen23@users.noreply.github.com>
    
    * Add search page
    
    Co-authored-by: Lukas Hagen <Greenscreen23@users.noreply.github.com>
    
    * Add notifications page
    
    Co-authored-by: Lukas Hagen <Greenscreen23@users.noreply.github.com>
    
    * Add profile page
    
    Co-authored-by: Lukas Hagen <Greenscreen23@users.noreply.github.com>
    
    * Rename users/registration_controller to users_controller
    This was requests by @Sohn123, as it did not make sense
    for the profile page to be managed by a registration controller
    
    Co-authored-by: Lukas Hagen <Greenscreen23@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
+  get 'dashboard', to: 'dashboard#index'
+  get 'search', to: 'search#index'
+  get 'notifications', to: 'notifications#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # https://github.com/heartcombo/devise/blob/main/README.md
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users' }
+  devise_scope :user do
+    get 'profile', to: 'users#profile'
+  end
 
   # Defines the root path route ("/")
   root "landing_page#index"
