4011379b2fe8f7dc5c163c9e1cf3afef87e3c2a3
Till-B <bergmann@uni-potsdam.de>
2022-11-20 12:05:16 +0100

    getter, setter for item price changed show file validation for for item instance variables

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,25 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :location, presence: true
+  validates :description, presence: true
+
+  def price_in_euro
+    if(price_ct != nil)
+      ct = price_ct%100
+      euro = (price_ct-ct)/100
+
+      return euro, ct
+    end
+
+    return 0,0
+
+  end
+
+  def price_in_euro=(euros)
+    self.price_ct = euros*100
+  end
+
 end
