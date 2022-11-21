244b32e1d6153d2184eb24e95b6eaf1b101cacf7
Richard Wohlbold <richard@rgwohlbold.de>
2022-11-21 16:16:34 +0100

    Add basic responsive navigation bar (#58)
    * Add basic responsive navigation bar
    
    * Test existence of navigation bar when logged in
    
    * Simplified HTML and SCSS and remove repetitions
    
    Co-authored-by: Aaron Schlitt <aaron@aaronschlitt.de>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :set_nav
+
+  def set_nav
+    @nav = [
+      { text: "Home", path: "#", icon: "bi bi-house-door" },
+      { text: "Search", path: "#", icon: "bi bi-search" },
+      { text: "New Item", path: "#", icon: "bi bi-plus-square" },
+      { text: "Notifications", path: "#", icon: "bi bi-bell" },
+      { text: "Profile", path: "#", icon: "bi bi-person" }
+    ]
+  end
 end
