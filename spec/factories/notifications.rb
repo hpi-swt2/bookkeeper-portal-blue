a7be57ca3daeb4145dfb66b35a9b4b2eb18f28a2
Jan Fehse <jan.fehse@student.hpi.uni-potsdam.de>
2022-11-21 14:55:29 +0100

    Added Tests for notifications list support

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :notification do
-    notification_snippet { "MyString" }
-    date { "2022-11-21 13:47:20" }
+    sequence(:notification_snippet) {|n| "Notification #{n}" }
+    sequence(:date) {|n| "2022-11-0#{n} 13:47:20" }
     user { FactoryBot.build(:user) }
   end
 end
--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -12,4 +12,11 @@ RSpec.describe Notification, type: :model do
     expect(notification.user).not_to be_blank
   end
   
+  it "has a factory that returns a list of different notifications" do
+    user = FactoryBot.build(:user)
+    notification_list = FactoryBot.build_list(:notification, 2, user: user)
+    expect(notification_list.length).to eq(2)
+    expect(notification_list[0].user).to eq(notification_list[1].user)
+    expect(notification_list[0]).not_to eq(notification_list[1])
+  end
 end
