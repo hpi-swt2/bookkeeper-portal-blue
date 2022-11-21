84067c67c71be35161f1e1f3e7f1d8cba6f88e82
Jacob Schäfer <js.jacobschaefer@gmail.com>
2022-11-21 21:42:48 +0100

    replace column owner with actual reference to the owner table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_202737) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_204035) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -50,10 +50,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_202737) do
     t.text "return_checklist"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "owner"
     t.string "holder"
-    t.integer "user_id", null: false
-    t.index ["user_id"], name: "index_items_on_user_id"
+    t.integer "owner", null: false
+    t.index ["owner"], name: "index_items_on_owner"
   end
 
   create_table "users", force: :cascade do |t|
@@ -70,5 +69,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_202737) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "items", "users"
+  add_foreign_key "items", "users", column: "owner"
 end
