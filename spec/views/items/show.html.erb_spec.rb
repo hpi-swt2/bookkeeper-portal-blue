6e40e0bebb789f90380f793b5b3be04aeccca63f
Jacob Schäfer <js.jacobschaefer@gmail.com>
2022-11-22 16:31:09 +0100

    use item factory in items show test

--- spec/views/items/show.html.erb_spec.rb
+++ spec/views/items/show.html.erb_spec.rb
@@ -2,16 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "items/show", type: :view do
   before do
-    assign(:item, Item.create!(
-                    name: "Name",
-                    category: "Category",
-                    location: "Location",
-                    description: "MyText",
-                    image: nil,
-                    price_ct: 2,
-                    rental_duration_sec: 3,
-                    return_checklist: "MyText"
-                  ))
+    assign(:item, create(:item))
   end
 
   it "renders attributes in <p>" do
@@ -19,10 +10,10 @@ RSpec.describe "items/show", type: :view do
     expect(rendered).to match(/Name/)
     expect(rendered).to match(/Category/)
     expect(rendered).to match(/Location/)
-    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/Description/)
     expect(rendered).to match(//)
     expect(rendered).to match(/2/)
     expect(rendered).to match(/3/)
-    expect(rendered).to match(/MyText/)
+    expect(rendered).to match(/MyChecklist/)
   end
 end
