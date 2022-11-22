b4d7d9825e0dad4f1bb4fe74f83ba8542dc94411
Jan Fehse <jan.fehse@student.hpi.uni-potsdam.de>
2022-11-22 12:46:17 +0100

    linting per Rubocop

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe Notification, type: :model do
     expect(notification).to be_valid
   end
 
-  it "should have a date and user" do
+  it "has a date and user" do
     expect(notification.date).not_to be_blank
     expect(notification.user).not_to be_blank
   end
-  
+
   it "has a factory that returns a list of different notifications" do
-    user = FactoryBot.build(:user)
-    notification_list = FactoryBot.build_list(:notification, 2, user: user)
+    user = build(:user)
+    notification_list = build_list(:notification, 2, user: user)
     expect(notification_list.length).to eq(2)
     expect(notification_list[0].user).to eq(notification_list[1].user)
     expect(notification_list[0]).not_to eq(notification_list[1])
