abd618da711e3f0d96789739c79e4ec45e5585ca
Jan Fehse <jan.fehse@student.hpi.uni-potsdam.de>
2022-11-23 15:08:02 +0100

    rubocop linting

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,9 +1,9 @@
 class NotificationsController < ApplicationController
-  
   before_action :authenticate_user!
   def index
     @notifications = Notification.where(user_id: current_user.id)
-    @dates = @notifications.group_by{|notification| notification.date.strftime('%d.%B %y')}.sort_by{|date, _| date}.reverse
-    
+    @dates = @notifications.group_by do |notification|
+               notification.date.strftime('%d.%B %y')
+             end.sort_by { |date, _| date }.reverse
   end
 end
--- spec/features/notifications/notifications_spec.rb
+++ spec/features/notifications/notifications_spec.rb
@@ -4,11 +4,11 @@ describe "Notifications Page", type: :feature do
   let(:password) { 'password' }
   let(:user) { create(:user, password: password) }
 
-  before :each do
+  before do
     sign_in user
     FactoryBot.reload
     @notifications = create_list(:notification, 5, user: user)
-    @notifications.each {|notification| notification.save}
+    @notifications.each(&:save)
   end
 
   it "cannot be reached when not logged in and displays error" do
@@ -18,32 +18,33 @@ describe "Notifications Page", type: :feature do
     expect(page).not_to have_current_path(notifications_path)
   end
 
-  it "can be reached when logged in" do 
+  it "can be reached when logged in" do
     visit notifications_path
+    expect(page).to have_current_path(notifications_path)
   end
 
   it "is scrollable" do
     pending("find out how to test, maybe just test that bar is always is always there")
-    fail
+    raise
   end
-  
-  it "displays the notifications of the current user with text" do 
+
+  it "displays the notifications of the current user with text" do
     visit notifications_path
-    for notification in @notifications do
+    @notifications.each do |notification|
       expect(page).to have_text(notification.notification_snippet)
     end
   end
 
   it "is grouped by date" do
-    same_day_notifications = create_list(:notification, 2, user: user, date: DateTime.now())
-    same_day_notifications.each {|notification| notification.save}
+    same_day_notifications = create_list(:notification, 2, user: user, date: DateTime.now)
+    same_day_notifications.each(&:save)
     visit notifications_path
-    for notification in @notifications do
+    @notifications.each do |notification|
       expect(page).to have_text(notification.date.strftime("%d.%B %y"))
     end
   end
 
-  it "is clickable" do 
+  it "is clickable" do
     visit notifications_path
     expect(page).to have_link @notifications[0].notification_snippet
   end
