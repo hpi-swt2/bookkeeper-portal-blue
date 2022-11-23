29e157c8af5f3c6a87cd5515d42cb89aa8fedf69
Aaron Schlitt <aaron@aaronschlitt.de>
2022-11-23 11:09:11 +0100

    Correct typos in test names

--- spec/features/translations/translation_spec.rb
+++ spec/features/translations/translation_spec.rb
@@ -4,18 +4,18 @@ require "rails_helper"
 describe "Translations", type: :feature do
 
 
-  it "displays correct title on homepage in english by default" do
+  it "displays correct title on homepage in English by default" do
     visit root_path
     expect(page).to have_text("Bookkeeper Blue")
   end
 
-  it "displays correct title on homepage in german when requested" do
+  it "displays correct title on homepage in German when requested" do
     page.driver.header 'Accept-language', 'de-DE'
     visit root_path
     expect(page).to have_text("Bookkeeper Blau")
   end
 
-  it "displays correct title on homepage in english when an invalid locale is requested" do
+  it "displays correct title on homepage in English when an invalid locale is requested" do
     page.driver.header 'Accept-language', 'pt-PT'
     visit root_path
     expect(page).to have_text("Bookkeeper Blue")
