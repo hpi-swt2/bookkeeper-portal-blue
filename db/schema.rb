864bfa24c72e80ca4516b67fe36f4c346ae85fda
JanFehse <73786484+JanFehse@users.noreply.github.com>
2022-11-24 11:50:10 +0100

    Feature/tf+ 30 notification page skeleton (#82)
    * Added a notification model and tests
    
    * Added Notifications with sorting by date with tests

--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_114920) do
     t.index ["owner"], name: "index_items_on_owner"
   end
 
+  create_table "notifications", force: :cascade do |t|
+    t.string "notification_snippet"
+    t.datetime "date"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_notifications_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -72,4 +81,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_114920) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users", column: "holder"
   add_foreign_key "items", "users", column: "owner"
+  add_foreign_key "notifications", "users"
 end
