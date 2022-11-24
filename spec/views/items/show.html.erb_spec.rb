3294370c03d23419be646abd1815e727b9b88982
antonneubauer <antongneubauer@gmail.com>
2022-11-24 21:56:43 +0100

    failing test for rendering edit button

--- spec/views/items/show.html.erb_spec.rb
+++ spec/views/items/show.html.erb_spec.rb
@@ -1,12 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "items/show", type: :view do
-  before do
-    assign(:item, create(:item))
-  end
+
+  let(:user) { create(:user) }
+  let(:user2) { create(:user) }
+  let(:item) { create(:item, owner: user.id) }
 
   it "renders attributes in <p>" do
-    render
+    render item
     expect(rendered).to match(/Name/)
     expect(rendered).to match(/Category/)
     expect(rendered).to match(/Location/)
@@ -15,5 +16,17 @@ RSpec.describe "items/show", type: :view do
     expect(rendered).to match(/2/)
     expect(rendered).to match(/3/)
     expect(rendered).to match(/MyChecklist/)
+  end
+
+  it "shows edit button for owner" do
+    sign_in user
+    render item
+    expect(rendered).to have_button(id: 'edit_item')
+  end
+
+  it "does not show edit button for non-owner" do
+    sign_in user2
+    render item
+    expect(rendered).not_to have_button(id: 'edit_item')
   end
 end
