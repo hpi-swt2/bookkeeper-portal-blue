9b92837f747ffdc2c6c352c3a3b7eed8bb6b8ebc
antonneubauer <antongneubauer@gmail.com>
2022-11-25 10:52:39 +0100

    further adopted tests

--- spec/views/items/show.html.erb_spec.rb
+++ spec/views/items/show.html.erb_spec.rb
@@ -1,32 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe "items/show", type: :view do
+RSpec.describe "items/show", type: :feature do
 
   let(:user) { create(:user) }
   let(:user2) { create(:user) }
   let(:item) { create(:item, owner: user.id) }
 
-  it "renders attributes in <p>" do
-    render item
-    expect(rendered).to match(/Name/)
-    expect(rendered).to match(/Category/)
-    expect(rendered).to match(/Location/)
-    expect(rendered).to match(/Description/)
-    expect(rendered).to match(//)
-    expect(rendered).to match(/2/)
-    expect(rendered).to match(/3/)
-    expect(rendered).to match(/MyChecklist/)
+  it "renders attributes" do
+    visit item_path(item)
+    expect(page).to have_text("Name")
+    expect(page).to have_text("Category")
+    expect(page).to have_text("Location")
+    expect(page).to have_text("Description")
+    expect(page).to have_text("Image")
+    expect(page).to have_text("Price")
+    expect(page).to have_text("Rental duration")
+    expect(page).to have_text("Rental start")
+    expect(page).to have_text("Return checklist")
   end
 
   it "shows edit button for owner" do
     sign_in user
-    render item
-    expect(rendered).to have_button(id: 'edit_item')
+    visit item_path(item)
+    expect(page).to have_button(id: "edit_item_button")
   end
 
   it "does not show edit button for non-owner" do
     sign_in user2
-    render item
-    expect(rendered).not_to have_button(id: 'edit_item')
+    visit item_path(item)
+    expect(page).not_to have_button(id: "destroy_item_button")
   end
 end
