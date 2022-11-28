20b7403bf51f20d6c1f3de07bf70e50de19b06a2
Noel-Bastubbe <82881590+Noel-Bastubbe@users.noreply.github.com>
2022-11-28 13:06:30 +0100

    Feature/mw-36-profile-page-skeleton (#90)
    * Add profile page skeleton with intended design
    * Add first_name, last_name and name to the model of the user
    * Add style conventions for profile page
    * Add edit profile button as icon
    * Add round profile picture placeholder
    * Add roles and groups as placeholder
    
    Co-authored-by: mariejarisch <marie.jarisch@student.hpi.uni-potsdam.de>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+# User model which has a first name, last name and full name
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -5,4 +6,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :notifications, dependent: :destroy
+
+  # Method expects all emails to follow format "firstname.lastname@anything" in order to extract first name out of email
+  def first_name
+    email.split("@")[0].split(".")[0].capitalize
+  end
+
+  # Method expects all emails to follow format "firstname.lastname@anything" in order to extract last name out of email
+  def last_name
+    email.split("@")[0].split(".")[1].capitalize
+  end
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
