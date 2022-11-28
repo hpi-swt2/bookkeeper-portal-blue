9a225ff2f433f456041b064251c848b8a89e50b4
Noel Bastubbe <noel.bastubbe@gmail.com>
2022-11-28 11:03:12 +0100

    * Add code comments * Change inline CSS to internal CSS for better readability

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # This method expects all emails to follow the format "firstname.lastname@anything" in order to extract the first name out of the email
   def first_name
     email.split("@")[0].split(".")[0].capitalize
   end
-
+  # This method expects all emails to follow the format "firstname.lastname@anything" in order to extract the last name out of the email
   def last_name
     email.split("@")[0].split(".")[1].capitalize
   end
