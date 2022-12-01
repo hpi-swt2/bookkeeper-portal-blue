6f3ad6c9f06f50b3f8fc34403520c5da496d46ba
Nils Hanff <45318774+Nils1729@users.noreply.github.com>
2022-12-01 19:36:42 +0100

    Reformulate tests to please linter

--- spec/features/landing_page/landing_page_spec.rb
+++ spec/features/landing_page/landing_page_spec.rb
@@ -6,13 +6,13 @@ RSpec.describe "Landing Page", type: :feature do
 
   it "redirects to login without user signed in" do
     visit root_path
-    expect(current_path).to eq(new_user_session_path)
+    expect(page).to have_current_path(new_user_session_path)
   end
 
   it "redirects to dashboard with user signed in" do
     @user = create(:user)
     sign_in @user
     visit root_path
-    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_current_path(dashboard_path)
   end
 end
