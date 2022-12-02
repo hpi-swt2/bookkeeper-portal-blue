b123d60425f26d456b4130208c09b8206b5a74e7
Marius Dörbandt <github.marius@doerbandt.de>
2022-12-02 20:25:31 +0100

    Add title and description to LendRequestNotification

--- app/models/lend_request_notification.rb
+++ app/models/lend_request_notification.rb
@@ -3,4 +3,12 @@ class LendRequestNotification < ApplicationRecord
 
   belongs_to :borrower, class_name: "User"
   belongs_to :item
+
+  def title
+    I18n.t "views.notifications.lend_request.title"
+  end
+
+  def description
+    I18n.t "views.notifications.lend_request.description", user: borrower.name, item: item.name
+  end
 end
