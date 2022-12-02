59b30160a24eb6972e3952a89655356e1f8ba02d
Marius Dörbandt <github.marius@doerbandt.de>
2022-12-02 20:16:24 +0100

    Remove notification snippet from Notification

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_114920) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_28_175122) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -57,7 +57,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_114920) do
   end
 
   create_table "notifications", force: :cascade do |t|
-    t.string "notification_snippet"
     t.datetime "date"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
