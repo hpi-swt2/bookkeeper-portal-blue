3c375fff950008a6b23124bb79e33c60a4119414
Marius Dörbandt <github.marius@doerbandt.de>
2022-12-02 20:26:17 +0100

    Add method delegation to specific Notification class

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,14 @@ class Notification < ApplicationRecord
   actable
 
   belongs_to :user
+
+  # delegate methods from the "subclasses" (which aren't really subclasses)
+  # to the specific instances
+  def method_missing(method, *args, &block)
+    if specific.self_respond_to?(method)
+      specific.send(method, *args, &block)
+    else
+      super
+    end
+  end
 end
