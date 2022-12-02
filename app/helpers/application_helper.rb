ad06d019ace815cf25d75f3ca17ca2a8168358f9
Marius Dörbandt <github.marius@doerbandt.de>
2022-12-02 19:17:13 +0100

    Add initial styles according to figma

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  include Rails.application.routes.url_helpers
+
+  def button_to(name = nil, options = nil, html_options = nil, &block)
+    new_html_options = add_class_name html_options, "btn"
+    super(name, options, new_html_options, &block)
+  end
+
+  def primary_button_to(name = nil, options = nil, html_options = nil, &block)
+    new_html_options = add_class_name html_options, "btn-primary"
+    button_to(name, options, new_html_options, &block)
+  end
+
+  def secondary_button_to(name = nil, options = nil, html_options = nil, &block)
+    new_html_options = add_class_name html_options, "btn-secondary"
+    button_to(name, options, new_html_options, &block)
+  end
+end
+
+
+def add_class_name(hash, class_name)
+  new_hash = hash
+  new_hash ||= {}
+  new_hash[:class] ||= ""
+  new_hash[:class] += " #{class_name}"
+  new_hash
 end
