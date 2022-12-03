15cc79a448484682bb65cd16cc97019ec3449b8a
mariejarisch <marie.jarisch@student.hpi.uni-potsdam.de>
2022-12-03 15:12:13 +0100

    Add testing of internationalization of the profile page

--- spec/features/users/profile_spec.rb
+++ spec/features/users/profile_spec.rb
@@ -9,10 +9,28 @@ RSpec.describe "Profile", type: :feature do
     expect(page).to have_text("max.mustermann@student.hpi.uni-potsdam.de")
   end
 
+  it "shows roles" do
+    sign_in user
+    visit profile_path
+    expect(page).to have_content I18n.t('views.profile.roles')
+  end
+
+  it "shows email address" do
+    sign_in user
+    visit profile_path
+    expect(page).to have_content I18n.t('views.profile.email')
+  end
+
   it "shows the user email" do
     sign_in user
     visit profile_path
     expect(page).to have_text("max.mustermann@student.hpi.uni-potsdam.de")
+  end
+
+  it "shows groups" do
+    sign_in user
+    visit profile_path
+    expect(page).to have_content I18n.t('views.profile.groups')
   end
 
   it "has an edit profile button" do
