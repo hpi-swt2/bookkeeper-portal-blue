fc8e08d588b598e0701422d0c4c1be12dd60b9ce
Lukas Hagen <43916057+Greenscreen23@users.noreply.github.com>
2022-12-06 16:12:03 +0100

    Add a validation such that each group has to have at least one owner

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,16 @@
+# Group Model which has a name and many members as well as owners
 class Group < ApplicationRecord
   has_many :memberships, dependent: :destroy
   has_many :members, through: :memberships, source: :user
 
   has_many :ownerships, class_name: 'Ownership', dependent: :destroy
   has_many :owners, through: :ownerships, source: :user
+
+  validate :owner?
+
+  private
+
+  def owner?
+    errors.add(:base, "Group has to have an owner") if owners.empty?
+  end
 end
--- spec/factories/groups.rb
+++ spec/factories/groups.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :group do
     name { "TestGroup" }
+    owners { [ FactoryBot.build(:user) ] }
   end
 end
