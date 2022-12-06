c212579dadb41f88ee7423b95cedea7aef948802
Bekay1211 <bekay12.11@gmail.com>
2022-12-06 16:55:18 +0100

    notificationview grouping and localisation done. Fixed Bug that caused dates to be sorted incorrectly

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,13 +2,14 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
   def index
     @active_notifications= Notification.where(active: true, user_id: current_user.id)
-    @inactive_notifications= Notification.where(active: false, user_id: current_user.id)
     @active_dates = @active_notifications.group_by do |notification|
-      notification.date.strftime('%d. %B %y')
+      notification.date.strftime('%y%m%d')
     end
     @active_dates = @active_dates.sort_by { |date, _| date }.reverse
+
+    @inactive_notifications= Notification.where(active: false, user_id: current_user.id)
     @inactive_dates = @inactive_notifications.group_by do |notification|
-      notification.date.strftime('%d. %B %y')
+      notification.date.strftime('%y%m%d')
     end
     @inactive_dates = @inactive_dates.sort_by { |date, _| date }.reverse
 
