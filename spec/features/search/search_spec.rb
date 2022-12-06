d229a7a4e99cfc247630bc7a710069b4a7381aca
antonykamp <info@antonykamp.de>
2022-12-06 11:43:34 +0100

    add end to end tests for search bar

---
+++ spec/features/search/search_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+describe "Seach Bar", type: :feature do
+
+  before do
+    @item_book = create(:item_book)
+    @item_beamer = create(:item_beamer)
+    @item_whiteboard = create(:item_whiteboard)
+    visit search_path
+  end
+
+  it "partial matching works for title" do
+    page.fill_in "search", with: "Ruby"
+    find('button[type="submit"]').click
+    expect(page).to have_text(@item_book.name)
+    expect(page).not_to have_text(@item_beamer.name)
+    expect(page).not_to have_text(@item_whiteboard.name)
+  end
+
+  it "partial matching works for description" do
+    page.fill_in "search", with: "book"
+    find('button[type="submit"]').click
+    expect(page).to have_text(@item_book.name)
+    expect(page).not_to have_text(@item_beamer.name)
+    expect(page).not_to have_text(@item_whiteboard.name)
+  end
+
+  it "partial matching works for empty string" do
+    page.fill_in "search", with: "Ruby"
+    find('button[type="submit"]').click
+    expect(page).not_to have_text(@item_book.name)
+    expect(page).not_to have_text(@item_beamer.name)
+    expect(page).not_to have_text(@item_whiteboard.name)
+  end
+
+end
