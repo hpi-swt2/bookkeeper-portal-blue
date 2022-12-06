6d119146959c2af0d9e17a686e714cb970cf1d28
Luke <37272151+SaturnHafen@users.noreply.github.com>
2022-12-06 17:28:57 +0100

    translation of search page (#125)
    Co-authored-by: Luke <37272151+SaturnHafen@users.noreply.github.com>
    Co-authored-by: Christian R <62649536+CR1337@users.noreply.github.com>

--- spec/features/search/search_spec.rb
+++ spec/features/search/search_spec.rb
@@ -1,6 +1,16 @@
 require "rails_helper"
 
 describe "Search page", type: :feature do
+  it "translates the close button to German" do
+    page.driver.header 'Accept-language', 'de-DE'
+    visit search_path
+    expect(page).to have_text("Schließen")
+  end
+
+  it "translates the close button to English by default" do
+    visit search_path
+    expect(page).to have_text("Close")
+  end
 
   it "shows all items when searching for nothing" do
     @item = create(:item)
