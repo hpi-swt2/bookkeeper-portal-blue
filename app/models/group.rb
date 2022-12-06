be64e064a9041161ed8114eb806e1315b2f2efca
Lukas Hagen <43916057+Greenscreen23@users.noreply.github.com>
2022-12-06 15:15:42 +0100

    Fix group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   has_many :memberships, dependent: :destroy
-  has_many :members, through: :memberships
+  has_many :members, through: :memberships, source: :user
 
   has_many :ownerships, class_name: 'Ownership', dependent: :destroy
   has_many :owners, through: :ownerships, source: :user
