2afcbae30b9507f889e7f28f5c4b04dabcf0452f
Nils1729 <45318774+Nils1729@users.noreply.github.com>
2022-12-08 13:53:43 +0100

    Feature/hghk 104 design and implement user grouping data model (#136)
    * Add models for groups and memberships
    
    * Create group spec and rename its users to members
    
    * Fix group model
    
    * Add a validation such that each group has to have at least one owner
    
    * Add a comment for promoting or demoting a user in a group
    
    * Add tests for the new group and membership models
    
    * Refactor group tests
    
    * Linting
    
    * Rename group test for owner list
    
    * Revert to normal STI for Membership and Ownership
    
    * Add the ability to promote or demote users in groups
    
    Co-authored-by: Nils Hanff <45318774+nils1729@users.noreply.github.com>
    
    * Update comment in group model
    
    Co-authored-by: Lukas Hagen <43916057+Greenscreen23@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_114920) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_05_160740) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,9 +34,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_114920) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "items", force: :cascade do |t|
@@ -54,6 +60,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_114920) do
     t.integer "holder"
     t.index ["holder"], name: "index_items_on_holder"
     t.index ["owner"], name: "index_items_on_owner"
+  end
+
+  create_table "memberships", force: :cascade do |t|
+    t.string "type"
+    t.integer "group_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["group_id", "user_id"], name: "index_memberships_on_group_id_and_user_id", unique: true
+    t.index ["group_id"], name: "index_memberships_on_group_id"
+    t.index ["user_id"], name: "index_memberships_on_user_id"
   end
 
   create_table "notifications", force: :cascade do |t|
@@ -81,5 +98,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_114920) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users", column: "holder"
   add_foreign_key "items", "users", column: "owner"
+  add_foreign_key "memberships", "groups"
+  add_foreign_key "memberships", "users"
   add_foreign_key "notifications", "users"
 end
---
+++ spec/models/group_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe Group, type: :model do
+
+  before do
+    @group = create(:group)
+  end
+
+  it "can be created using a factory" do
+    expect(@group).to be_valid
+  end
+
+  it "is not valid without an owner" do
+    @group.owners.delete_all
+    expect(@group).not_to be_valid
+  end
+
+  it "lists owners in the list of members" do
+    user = build(:user)
+    @group.owners.append(user)
+
+    expect(@group.owners).to include(user)
+    expect(@group.members).to include(user)
+  end
+
+  it "only list owners in the list of owners" do
+    user = build(:user)
+    @group.members.append(user)
+
+    expect(@group.members).to include(user)
+    expect(@group.owners).not_to include(user)
+  end
+
+  it "includes a user at most once" do
+    user = build(:user)
+    @group.members.append(user)
+
+    expect { @group.members.append(user) }.to raise_error(ActiveRecord::RecordNotUnique)
+    expect { @group.owners.append(user) }.to raise_error(ActiveRecord::RecordNotUnique)
+  end
+end
