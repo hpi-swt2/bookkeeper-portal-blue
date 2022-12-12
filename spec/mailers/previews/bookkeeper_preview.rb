ec5f818ca521c2e5d52784afd2081cf67778e790
Jacob Schäfer <js.jacobschaefer@gmail.com>
2022-12-12 18:29:04 +0100

    fix linter

--- spec/mailers/previews/bookkeeper_preview.rb
+++ spec/mailers/previews/bookkeeper_preview.rb
@@ -1,9 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/bookkeeper
 class BookkeeperPreview < ActionMailer::Preview
-
   # Preview this email at http://localhost:3000/rails/mailers/bookkeeper/notification
   def notification
     BookkeeperMailer.notification
   end
-
 end
