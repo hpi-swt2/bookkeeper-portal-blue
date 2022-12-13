5743e90675a0a53c905e1a31e51c14e59d74d432
Alexander Ungefug <82446024+AlexanderUngefug@users.noreply.github.com>
2022-12-13 10:40:48 +0100

    Feature/pbrh+ 50 return item (#156)
    * Fix notifications sorting
    
    * Clean up notifications index page
    
    * Remove notification snippet from Notification
    
    * Add LendRequestNotification
    
    * Make Notification actable
    
    * Let LendRequestNotification act_as a Notification
    
    * Add title and description to LendRequestNotification
    
    * Add method delegation to specific Notification class
    
    * Show a notification's title and description
    
    * Add ability to show a custom partial for notifications
    
    * Add dummy buttons for LendRequestNotification
    
    * implementing return item functionality
    
    * fixed validation for lend_status
    
    * removed empty lines in item controller
    
    * Notifications implemented
    
    * Add tests for LendRequestNotification
    
    * implementing tests
    
    * add more tests and cleanup code
    
    * minor test fix
    
    * lint using rubocop --fix
    
    * fix factorybot error
    
    * fix dashboard test and lint using rubocop
    
    * Modal Window added to return Notifications
    
    * set image size in item#index to max 300 px
    
    * merge with dev
    
    * Fix return notification test
    
    * added empty line between method defintions
    
    * removed whitespace in Notification
    
    * fixed endless loop
    
    * fixing CI pipeline
    
    * Probably (?) fix weird bug
    
    Co-authored-by: Marius Dörbandt <github.marius@doerbandt.de>
    Co-authored-by: Jannik Huschka <huschka@neuland.dev>
    Co-authored-by: Oliver Schulz <oliver.schulz@student.hpi.de>

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,8 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :owner, presence: true
   validates :price_ct, numericality: { allow_nil: true, greater_than_or_equal_to: 0 }
+  enum :lend_status, { available: 0, lent: 1, pending_return: 2 }
+  validates :lend_status, presence: true, inclusion: { in: lend_statuses.keys }
 
   def price_in_euro
     unless price_ct.nil?
@@ -19,6 +21,22 @@ class Item < ApplicationRecord
     end
 
     [0, 0]
+  end
+
+  def request_return
+    # TODO: send request return notification to owner with holder information
+    self.lend_status = :pending_return
+  end
+
+  def accept_return
+    self.rental_start = nil
+    self.rental_duration_sec = nil
+    self.holder = nil
+    self.lend_status = :available
+  end
+
+  def deny_return
+    # TODO
   end
 
   def price_in_euro=(euros)
