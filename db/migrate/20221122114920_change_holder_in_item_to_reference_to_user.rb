9510e442bdbce20a49ad79ec83045d972865ef46
Richard Wohlbold <richard@rgwohlbold.de>
2022-12-14 18:18:09 +0100

    Resolve duplicate foreign key error

--- db/migrate/20221122114920_change_holder_in_item_to_reference_to_user.rb
+++ db/migrate/20221122114920_change_holder_in_item_to_reference_to_user.rb
@@ -1,7 +1,7 @@
 class ChangeHolderInItemToReferenceToUser < ActiveRecord::Migration[7.0]
   def change
     remove_column :items, :holder, :string
-    add_reference :items, :user, null: true, foreign_key: true
-    rename_column :items, :user_id, :holder
+    add_reference :items, :holder, null: true, foreign_key: { to_table: :users }
+    rename_column :items, :holder_id, :holder
   end
 end
