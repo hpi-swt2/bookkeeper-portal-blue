fdcd5c9a7e80abd9c4d34a53b71823657b66c712
Jakob Timm <93190743+JakobTimm@users.noreply.github.com>
2022-12-14 19:34:48 +0100

    rspec fixed (factories needed :active properties) Co-authored-by: kohlros <kohlros@uni-potsdam.de>

--- spec/factories/return_request_notifications.rb
+++ spec/factories/return_request_notifications.rb
@@ -4,10 +4,12 @@ FactoryBot.define do
     date { Time.zone.now }
     item { FactoryBot.build(:pending, owner: user.id) }
     borrower { FactoryBot.build(:max) }
+    sequence(:active) { |n| n % 2 }
   end
 
   factory :invalid_return_request_notification, class: 'ReturnRequestNotification' do
     item { FactoryBot.build(:pending) }
     borrower { FactoryBot.build(:max) }
+    sequence(:active) { |n| n % 2 }
   end
 end
