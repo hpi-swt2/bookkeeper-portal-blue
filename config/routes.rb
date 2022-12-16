97fbe413273b66405eb399ac6f7b8dcb712922bc
Noel <noel.bastubbe@gmail.com>
2022-12-16 09:23:26 +0100

    * Fixes Bug 151 Profile throws error when user is not logged in * Changes dashboard behavior when user not logged in to redirect to login page

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,11 @@ Rails.application.routes.draw do
   get 'dashboard', to: 'dashboard#index'
   get 'search', to: 'search#index'
   get 'notifications', to: 'notifications#index'
+  get 'profile', to: 'profile#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # https://github.com/heartcombo/devise/blob/main/README.md
   devise_for :users, controllers: { registrations: 'users' }
-  devise_scope :user do
-    get 'profile', to: 'users#profile'
-  end
 
   resources :users
   post 'add_to_waitlist/:id', to: 'items#add_to_waitlist', as: 'add_to_waitlist'
