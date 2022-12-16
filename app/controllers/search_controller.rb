2c635be9c076f4b01a67c5ed14ad6a6c947e75d8
antonykamp <info@antonykamp.de>
2022-12-16 10:04:24 +0100

    add hidden lastsearch field
    Co-authored-by: SaturnHafen <37272151+SaturnHafen@users.noreply.github.com>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,12 @@
 class SearchController < ApplicationController
   def index
     search_term = params[:search]
+    if search_term.blank?
+      @lastsearch = params[:lastsearch]
+      search_term = @lastsearch
+    else
+      @lastsearch = search_term
+    end
     @results = helpers.search_for_items(search_term)
   end
 end
