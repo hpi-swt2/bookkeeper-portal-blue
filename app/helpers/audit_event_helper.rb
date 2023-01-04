62ef95540dd1d4982c0e0429b1644df8147aead4
antonykamp <info@antonykamp.de>
2023-01-04 07:07:48 +0100

    Add audit event helper
    Introduces the audit event helper as interface for audit event. User are aware of available audit event types now.

---
+++ app/helpers/audit_event_helper.rb
@@ -0,0 +1,41 @@
+module AuditEventHelper
+  def audit_create_item(item)
+    create_audit_event(item, :create_item)
+  end
+
+  def audit_request_lend(item)
+    create_audit_event(item, :request_lend)
+  end
+
+  def audit_accept_lend(item)
+    create_audit_event(item, :accept_lend)
+  end
+
+  def audit_request_return(item)
+    create_audit_event(item, :request_return)
+  end
+
+  def audit_accept_return(item)
+    create_audit_event(item, :accept_return)
+  end
+
+  def audit_deny_return(item)
+    create_audit_event(item, :deny_return)
+  end
+
+  def audit_add_to_waitlist(item)
+    create_audit_event(item, :add_to_waitlist)
+  end
+
+  def audit_leave_waitlist(item)
+    create_audit_event(item, :leave_waitlist)
+  end
+
+  private
+
+  def create_audit_event(item, event_type)
+    audit_event = AuditEvent.new(item: item, owner_id: item.owner, holder_id: item.holder,
+                                 triggering_user: current_user, event_type: event_type)
+    audit_event.save
+  end
+end
