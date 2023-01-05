d1b3f8025bea7762163953344dabf99e1b3bab75
Bekay1211 <bekay12.11@gmail.com>
2023-01-05 19:00:11 +0100

    made borrower gets notified when request is accepted test more specific by checking if 'Lending Accepted' exists

--- spec/features/notifications/lend_request_notification_spec.rb
+++ spec/features/notifications/lend_request_notification_spec.rb
@@ -57,5 +57,6 @@ describe "Lend Request Notifications", type: :feature do
     visit notifications_path
     expect(page).to have_text(user.name)
     expect(page).to have_text(item.name)
+    expect(page).to have_text('Lending Accepted')
   end
 end
