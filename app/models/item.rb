fbc7d695d1e9467d6dfa64392afaae84c6a5e3f7
Noel <noel.bastubbe@gmail.com>
2023-01-05 18:45:19 +0100

    * Merges #70 Component - Dashboard: Lent Items - Highlighting * Merges and Fixes #38 Component - Dashboard: Unread Messages * Merges dev

--- app/models/item.rb
+++ app/models/item.rb
@@ -99,14 +99,6 @@ class Item < ApplicationRecord
     end
   end
 
-  def rental_end
-    rental_start + rental_duration_sec
-  end
-
-  def remaining_rental_duration
-    rental_end - Time.now.utc
-  end
-
   def progress_lent_time
     lent_time_progress = (((rental_duration_sec - remaining_rental_duration) * 100) / rental_duration_sec).to_i
     if lent_time_progress.negative?
@@ -115,22 +107,6 @@ class Item < ApplicationRecord
       100
     else
       lent_time_progress
-    end
-  end
-
-  def print_remaining_rental_duration
-    print_time_from_seconds(remaining_rental_duration)
-  end
-
-  def print_time_from_seconds(seconds)
-    if seconds.negative?
-      I18n.t("views.dashboard.lent_items.expired", date: rental_end.strftime("%d.%m.%Y"))
-    elsif seconds < 86_400
-      I18n.t "views.dashboard.lent_items.today"
-    elsif seconds < 7 * 86_400
-      I18n.t("views.dashboard.lent_items.days", count: (seconds / 86_400).to_i)
-    else
-      I18n.t("views.dashboard.lent_items.weeks", count: (seconds / (7 * 86_400)).to_i)
     end
   end
 end
--- spec/features/dashboard/dashboard_spec.rb
+++ spec/features/dashboard/dashboard_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe "Dashboard", type: :feature do
     visit dashboard_path
     expect(page).to have_link(href: '/notifications')
     expect(page).to have_content I18n.t('views.dashboard.unread_messages', num: 0)
-    @notifications = create_list(:lend_request_notification, 2, user: user, item: item, borrower: borrower)
+    @notifications = create_list(:lend_request_notification, 2, receiver: user, item: item, borrower: borrower)
     @notifications.each(&:save)
     page.refresh
     expect(page).to have_content I18n.t('views.dashboard.unread_messages', num: 2)
