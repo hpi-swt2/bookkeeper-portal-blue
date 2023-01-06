58be265aa551e27e80feed29156c031d125c5dcf
antonneubauer <antongneubauer@gmail.com>
2023-01-07 00:35:46 +0100

    Revert "Fixed bug for confirm pickup crashing everything (#233)"
    This reverts commit 2a05d89f2c190ab9bf0b0af1d5a960b7a1b2c89a.

--- spec/features/request/accept_lend_request_spec.rb
+++ spec/features/request/accept_lend_request_spec.rb
@@ -28,22 +28,22 @@ describe "Requests handling", type: :feature do
 
   # TODO: fix this test! It fails because there is no Check button is renamed to the notification title
 
-  it "if borrower confirms pickup, the item lend status changes to lent" do
-    owner = create(:max)
-    borrower = create(:peter)
-    item = create(:item, owner: owner.id)
-    sign_in borrower
-    visit item_path(item)
-    click_button('Lend')
-    sign_in owner
-    visit notifications_path
-    click_on('Lend Request')
-    click_button('Accept')
-    sign_in borrower
-    visit item_path(item)
-    click_button('Confirm pickup')
-    puts item.reload.lend_status
-    expect(item.reload.lend_status).to eq('lent')
-  end
+  # it "if borrower confirms pickup, the item lend status changes to lent" do
+  #  owner = create(:max)
+  #  borrower = create(:peter)
+  #  item = create(:item, owner: owner.id)
+  #  sign_in borrower
+  #  visit item_path(item)
+  #  click_button('Lend')
+  #  sign_in owner
+  #  visit notifications_path
+  #  click_button('Check')
+  #  click_button('Accept')
+  #  sign_in borrower
+  #  visit item_path(item)
+  #  click_button('Confirm pickup')
+  #  puts item.reload.lend_status
+  #  expect(item.reload.lend_status).to eq('lent')
+  # end
 
 end
