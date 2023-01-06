5c7033c26a5ee5af297c78873524fb1e1d0e1778
antonneubauer <antongneubauer@gmail.com>
2023-01-07 00:35:46 +0100

    Revert "OIDC Login (#201)"
    This reverts commit d786d3495a35fee8dd68de4ebe5e6b2adfe348cc.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,7 +57,4 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
-
-  # Enable testing for OIDC
-  OmniAuth.config.test_mode = true
 end
