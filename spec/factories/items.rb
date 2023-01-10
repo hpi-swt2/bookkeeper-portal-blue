09c017462170edc9c1e867d71e7dc84a11c6d163
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2023-01-10 16:37:54 +0100

    Add item permission tests

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -12,6 +12,19 @@ FactoryBot.define do
     lend_status { :available }
     owning_user { create(:user) }
   end
+  factory :item_owned_by_group, class: 'Item' do
+    name { "MyName" }
+    category { "MyCategory" }
+    location { "MyLocation" }
+    description { "MyDescription" }
+    image { Rack::Test::UploadedFile.new('spec/testimages/test_image.png', 'image/png') }
+    price_ct { 1 }
+    rental_duration_sec { 1 }
+    rental_start { "2022-11-18 15:32:07" }
+    return_checklist { "MyChecklist" }
+    lend_status { :available }
+    owning_group { create(:group) }
+  end
   factory :pending, class: 'Item' do
     name { "MyName2" }
     category { "MyCategory" }
