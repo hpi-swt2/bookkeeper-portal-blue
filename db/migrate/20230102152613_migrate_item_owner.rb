ac6f144fbb9f29a69d375fca509692272d624003
Lukas Radermacher <49586507+lukasrad02@users.noreply.github.com>
2023-01-10 15:55:31 +0100

    Remove comment
    Co-authored-by: Nils Hanff <45318774+nils1729@users.noreply.github.com>

--- db/migrate/20230102152613_migrate_item_owner.rb
+++ db/migrate/20230102152613_migrate_item_owner.rb
@@ -1,7 +1,6 @@
 class MigrateItemOwner < ActiveRecord::Migration[7.0]
   def change
     Item.all.each do |item|
-      # item.update_attribute(owning_user: User.find(item.owner))
       item.owning_user = User.find(item.owner)
     end
     remove_foreign_key :items, :users, column: :owner
