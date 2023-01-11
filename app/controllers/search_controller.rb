4b692b78b013dc1a95814b8570d56bd965902272
Luke <saturnhafen@gmail.com>
2023-01-11 16:10:55 +0100

    Add internationalization Co-authored-by: Lietze <50149435+Lietze@users.noreply.github.com>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,6 @@
 class SearchController < ApplicationController
   def index
-    @availability_options = [["unavailable", 0], ["available", 1]]
-    @category_options = Item.select(:category).distinct.pluck(:category)
+    setup_variables
 
     search_term = params[:search]
     if search_term.blank?
@@ -11,5 +10,13 @@ class SearchController < ApplicationController
       @lastsearch = search_term
     end
     @results = helpers.search_for_items(search_term)
+  end
+
+  private
+
+  def setup_variables
+    @availability_options = [[t('views.search.filter_modal.available'), 0],
+                             [t('views.search.filter_modal.unavailable'), 1]]
+    @category_options = Item.select(:category).distinct.pluck(:category)
   end
 end
