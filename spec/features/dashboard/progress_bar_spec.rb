587aa1059e64ee63a8d9b1eca64c94d9a50b2de8
Noel-Bastubbe <82881590+Noel-Bastubbe@users.noreply.github.com>
2023-01-11 14:36:41 +0100

    Fix/mw 38 39 72 adds missing tests to existing features (#263)
    * Adds tests for all offered items tags
    * Adds tests for all lent items tags
    * Adds tests for the progress bars under the lent items
    * Refactors the large unread message test into 3 smaller tests which tests the same features
    
    * Extracts offered items tests from dashboard_spec.rb to offered_items_spec.rb
    * Extracts lents items tests from dashboard_spec.rb to lent_items_spec.rb
    * Extracts progress bar under lents items tests from dashboard_spec.rb to progress_bar_spec.rb
    * Extracts wishlist tests from dashboard_spec.rb to wishlist_spec.rb

---
+++ spec/features/dashboard/progress_bar_spec.rb
@@ -0,0 +1,69 @@
+require "rails_helper"
+
+RSpec.describe "Progress Bar under Lent items", type: :feature do
+
+  let(:password) { 'password' }
+  let(:user) { create(:user, password: password) }
+  let(:item) { create(:item, owning_user: user) }
+
+  it "displays the progess as 0 if the rental start is in the future" do
+    @owner = create(:user)
+    @user = create(:user)
+    item = create(:item, owning_user: @owner, holder: @user.id, rental_start: Time.now.utc + 1.day)
+    sign_in @user
+    visit dashboard_path
+    expect(page).to have_content(item.name)
+    expect(find('div.progress-bar')['aria-valuenow']).to eq('0%')
+  end
+
+  it "displays the progess as 0 if the rental start is now" do
+    @owner = create(:user)
+    @user = create(:user)
+    item = create(:item, owning_user: @owner, holder: @user.id, rental_start: Time.now.utc, rental_duration_sec: 86_400)
+    sign_in @user
+    visit dashboard_path
+    expect(page).to have_content(item.name)
+    expect(find('div.progress-bar')['aria-valuenow']).to eq('0%')
+  end
+
+  it "displays the progess as 100 if the remaining rental duration is zero" do
+    @owner = create(:user)
+    @user = create(:user)
+    item = create(:item, owning_user: @owner, holder: @user.id, rental_duration_sec: 0)
+    sign_in @user
+    visit dashboard_path
+    expect(page).to have_content(item.name)
+    expect(find('div.progress-bar')['aria-valuenow']).to eq('100%')
+  end
+
+  it "displays the progess as 50 if the remaining rental duration is half of the original rental duration" do
+    @owner = create(:user)
+    @user = create(:user)
+    item = create(:item, owning_user: @owner, holder: @user.id, rental_duration_sec: 172_800,
+                         rental_start: Time.now.utc - 1.day)
+    sign_in @user
+    visit dashboard_path
+    expect(page).to have_content(item.name)
+    expect(find('div.progress-bar')['aria-valuenow']).to eq('50%')
+  end
+
+  it "displays the progess as 100 if the rental start is nil" do
+    @owner = create(:user)
+    @user = create(:user)
+    item = create(:item, owning_user: @owner, holder: @user.id, rental_start: nil)
+    sign_in @user
+    visit dashboard_path
+    expect(page).to have_content(item.name)
+    expect(find('div.progress-bar')['aria-valuenow']).to eq('100%')
+  end
+
+  it "displays the progess as 100 if the remaining rental duration is nil" do
+    @owner = create(:user)
+    @user = create(:user)
+    item = create(:item, owning_user: @owner, holder: @user.id, rental_duration_sec: nil)
+    sign_in @user
+    visit dashboard_path
+    expect(page).to have_content(item.name)
+    expect(find('div.progress-bar')['aria-valuenow']).to eq('100%')
+  end
+end
