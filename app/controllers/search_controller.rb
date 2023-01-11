d7e2225e087bec66d11066ab60a66a2ca2e8d1cb
Luke <saturnhafen@gmail.com>
2023-01-11 17:16:17 +0100

    Add basic filtering logic

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,7 +9,9 @@ class SearchController < ApplicationController
     else
       @lastsearch = search_term
     end
-    @results = helpers.search_for_items(search_term)
+    parse_filters
+
+    @results = helpers.search_for_items(search_term, @filters)
   end
 
   private
@@ -18,5 +20,20 @@ class SearchController < ApplicationController
     @availability_options = [[t('views.search.filter_modal.available'), 0],
                              [t('views.search.filter_modal.unavailable'), 1]]
     @category_options = Item.select(:category).distinct.pluck(:category)
+  end
+
+  def parse_filters
+    availability = params[:availability]
+    category = params[:category]
+
+    @filters = {}
+
+    if availability.present? && availability.to_i <= 1 && availability.to_i >= 0
+      @filters[:lend_status] = availability.to_i
+    end
+
+    return if category.blank?
+
+    @filters[:category] = category
   end
 end
