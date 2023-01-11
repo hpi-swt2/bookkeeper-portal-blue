a2b443304e649eee2a0601dcc6c0a6753ab5d7f5
Noel <noel.bastubbe@gmail.com>
2023-01-11 13:49:46 +0100

    * Extracts wishlist tests from dashboard_spec.rb to wishlist_spec.rb

--- spec/features/dashboard/dashboard_spec.rb
+++ spec/features/dashboard/dashboard_spec.rb
@@ -57,48 +57,4 @@ RSpec.describe "Dashboard", type: :feature do
     visit dashboard_path
     expect(page).to have_content I18n.t('views.dashboard.wishlist.title')
   end
-
-  it "shows wishlist item" do
-    @user = create(:user)
-    item = create(:item)
-    @user.wishlist << (item)
-    sign_in @user
-    visit dashboard_path
-    expect(page).to have_content(item.name)
-  end
-
-  it "shows message when wishlist is empty" do
-    @user = create(:user)
-    sign_in @user
-    visit dashboard_path
-    expect(page).to have_content I18n.t('views.dashboard.wishlist.missing_wishlist')
-  end
-
-  it "shows the correct tag for available wishlist items" do
-    @user = create(:user)
-    item = create(:item)
-    @user.wishlist << (item)
-    sign_in @user
-    visit dashboard_path
-    expect(page).to have_content I18n.t('views.dashboard.wishlist.available')
-  end
-
-  it "shows the correct tag for unavailable wishlist items" do
-    @user = create(:user)
-    @holder = create(:user)
-    item = create(:item, holder: @holder.id)
-    @user.wishlist << (item)
-    sign_in @user
-    visit dashboard_path
-    expect(page).to have_content I18n.t('views.dashboard.wishlist.not_available')
-  end
-
-  it "shows the correct tag for lend wishlist items" do
-    @user = create(:user)
-    item = create(:item, holder: @user.id)
-    @user.wishlist << (item)
-    sign_in @user
-    visit dashboard_path
-    expect(page).to have_content I18n.t('views.dashboard.wishlist.lent_by_you')
-  end
 end
