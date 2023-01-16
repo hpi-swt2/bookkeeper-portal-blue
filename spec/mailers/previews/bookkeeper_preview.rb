bf5abeeaf7c6a32594a1b907f50ef03b9ad8acd0
Jacob Schäfer <js.jacobschaefer@gmail.com>
2023-01-16 11:17:33 +0100

    remove bookkeeper mailer

--- spec/mailers/previews/bookkeeper_preview.rb
+++
@@ -1,7 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/bookkeeper
-class BookkeeperPreview < ActionMailer::Preview
-  # Preview this email at http://localhost:3000/rails/mailers/bookkeeper/notification
-  def notification
-    BookkeeperMailer.notification
-  end
-end
