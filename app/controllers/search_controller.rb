c46038691a8785fbf4355e94cf2dcadf3a2743f8
antonykamp <info@antonykamp.de>
2023-01-17 10:47:33 +0100

    fix rubocop

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    search_term = params[:search]  
+    search_term = params[:search]
     @results = helpers.search_for_items(search_term)
   end
 end
