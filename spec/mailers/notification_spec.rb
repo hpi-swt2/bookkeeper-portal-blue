e812fd246efbdf70cf8ecc30b5b98e1eda5ff7f9
Jacob Schäfer <js.jacobschaefer@gmail.com>
2023-01-18 18:25:29 +0100

    test that mail renders header and body

--- spec/mailers/notification_spec.rb
+++ spec/mailers/notification_spec.rb
@@ -2,16 +2,20 @@ require "rails_helper"
 
 RSpec.describe NotificationMailer, type: :mailer do
   describe "notification" do
-    let(:mail) { NotificationMailer.notification }
+    before do
+      @notification = build(:lend_request_notification)
+    end
+
+    let(:mail) { NotificationMailer.notification(@notification) }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Notification")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+      expect(mail.subject).to eq("New Notification: Lend Request")
+      expect(mail.to).to eq([@notification.receiver.email])
+      expect(mail.from).to eq(["bookkeeperblue.notification@gmail.com"])
     end
 
     it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.body.encoded).to match(@notification.description)
     end
   end
 
