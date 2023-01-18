a84dd1c718480b631b46ec5c343a9b01a8fcabd7
Jacob Schäfer <js.jacobschaefer@gmail.com>
2023-01-18 18:44:04 +0100

    create factory for move_up_on_waitlist_notifications

--- spec/factories/move_up_on_waitlist_notifications.rb
+++ spec/factories/move_up_on_waitlist_notifications.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :move_up_on_waitlist_notification do
-    item { nil }
-  end
+      sequence(:active) { |n| n % 2 }
+      sequence(:date) { |n| Time.strptime("2022-11-0#{(n % 9) + 1} 13:47:20", "%Y-%m-%d %H:%M:%S") }
+      sequence(:unread) { true }
+      receiver { FactoryBot.build(:user) }
+      item { FactoryBot.build(:item) }
+    end
 end
