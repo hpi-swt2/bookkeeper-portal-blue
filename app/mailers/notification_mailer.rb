09381b5fdbb98e5636f1766aeb12dd78685acdcf
Jacob Schäfer <js.jacobschaefer@gmail.com>
2023-01-18 17:49:32 +0100

    include description of notification in email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,9 +1,10 @@
 class NotificationMailer < ApplicationMailer
 
   def notification(notification)
-    @greeting = "Hi"
+    @title = notification.title
+    @description = notification.description
 
     mail(to: notification.receiver.email,
-         subject: I18n.t("notification_mailer.notification.subject", notification_name: notification.title))
+         subject: I18n.t("notification_mailer.notification.subject", notification_name: @title))
   end
 end
