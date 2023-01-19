ed9a3c168bc19160f6dc1656cda4785a27f981fd
Franka-T <frankatraupe@hotmail.com>
2023-01-19 10:30:33 +0100

    first ideas #251

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,4 +29,15 @@ module ApplicationHelper
   rescue ActionView::MissingTemplate
     # do nothing
   end
+
+  def language_flag(locale)
+    if locale == :en
+        updated_locale = :us
+      else
+        updated_locale = locale
+      end
+    content_class = "flag flag-#{updated_locale}"
+    content_tag(:span, nil, class: content_class)
+  end
+
 end
