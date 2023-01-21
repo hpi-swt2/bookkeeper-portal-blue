521732256b60adf8cda16f12ff110a2c047f51e8
Nils Hanff <45318774+Nils1729@users.noreply.github.com>
2023-01-21 15:44:33 +0100

    Add lost migration from #305

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_21_122621) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_21_122621) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
@@ -158,6 +158,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_21_122621) do
     t.index ["item_id", "user_or_group_id", "user_or_group_type"], name: "index_permission_on_item_and_user_or_group", unique: true
     t.index ["item_id"], name: "index_permissions_on_item_id"
     t.index ["user_or_group_type", "user_or_group_id"], name: "index_permissions_on_user_or_group"
+  end
+
+  create_table "removed_from_group_notifications", force: :cascade do |t|
+    t.string "group_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "return_accepted_notifications", force: :cascade do |t|
