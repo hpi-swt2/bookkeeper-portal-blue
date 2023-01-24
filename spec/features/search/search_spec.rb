51eda2fbeb32c6ecd35d023de91958130e9485b9
Luke <37272151+SaturnHafen@users.noreply.github.com>
2023-01-24 14:28:53 +0100

    Feature: #19 Filter Options (#265)
    * Change layout to mockup
    
    * Fix a typo
    
    Co-authored-by: Lietze <max.lietze@t-online.de>
    
    * Add internationalization
    
    Co-authored-by: Lietze <50149435+Lietze@users.noreply.github.com>
    
    * Add basic filtering logic
    
    * Add basic search filters
    
    * Remove view test
    
    * Migrate availabilityfilter to numerical
    
    Co-authored-by: CR1337 <62649536+CR1337@users.noreply.github.com>
    
    * Add tests
    
    * Reference the filter creation
    
    * Fix rubocop
    
    * Allow filtering when searchbar is empty
    
    * Add more tests
    
    Co-authored-by: Lietze <50149435+Lietze@users.noreply.github.com>
    
    * Keep filters selected
    
    * Add even more tests
    
    Co-authored-by: Lietze <50149435+Lietze@users.noreply.github.com>
    
    * Add "Everything" filter option
    
    Co-authored-by: Lietze <50149435+Lietze@users.noreply.github.com>
    
    Co-authored-by: CR1337 <62649536+CR1337@users.noreply.github.com>
    Co-authored-by: Lietze <50149435+Lietze@users.noreply.github.com>

--- spec/features/search/search_spec.rb
+++ spec/features/search/search_spec.rb
@@ -41,6 +41,16 @@ describe "Search page", type: :feature do
     expect(page).not_to have_text(@item_whiteboard.name)
   end
 
+  it "keeps the result the same when searching multiple times" do
+    page.fill_in "search", with: "book"
+    click_button("submit")
+    click_button("submit")
+
+    expect(page).to have_text(@item_book.name)
+    expect(page).not_to have_text(@item_beamer.name)
+    expect(page).not_to have_text(@item_whiteboard.name)
+  end
+
   it "partial matching works for empty string" do
     page.fill_in "search", with: ""
     click_button("submit")
@@ -63,6 +73,67 @@ describe "Search page", type: :feature do
     expect(page).to have_text(@item_book.name)
     expect(page).to have_text(@item_beamer.name)
     expect(page).to have_text(@item_whiteboard.name)
+  end
+
+  it "shows only the items with the correct category" do
+    page.fill_in "search", with: "b"
+    click_button("filter")
+    select(@item_book.category, from: "category").select_option
+    click_button("submit")
+
+    expect(page).to have_text(@item_book.name)
+    expect(page).not_to have_text(@item_beamer.name)
+    expect(page).not_to have_text(@item_whiteboard.name)
+  end
+
+  it "shows only unavailable items when filtering for unavailability" do
+    page.fill_in "search", with: "b"
+    click_button("filter")
+    select("Unavailable", from: "availability").select_option
+    click_button("submit")
+
+    expect(page).to have_text(@item_book.name)
+    expect(page).to have_text(@item_beamer.name)
+    expect(page).not_to have_text(@item_whiteboard.name)
+  end
+
+  it "shows only available items when seraching for blank and having an availability filter" do
+    select("Available", from: "availability").select_option
+    click_button("submit")
+
+    expect(page).not_to have_text(@item_book.name)
+    expect(page).not_to have_text(@item_beamer.name)
+    expect(page).to have_text(@item_whiteboard.name)
+  end
+
+  it "applies both filters" do
+    select("Unavailable", from: "availability").select_option
+    select(@item_book.category, from: "category").select_option
+    click_button("submit")
+
+    expect(page).to have_text(@item_book.name)
+    expect(page).not_to have_text(@item_beamer.name)
+    expect(page).not_to have_text(@item_whiteboard.name)
+  end
+
+  it "keeps the filter selected" do
+    select(@item_beamer.category, from: "category").select_option
+    select("Unavailable", from: "availability").select_option
+
+    click_button("submit")
+    click_button("submit")
+
+    expect(page).not_to have_text(@item_book.name)
+    expect(page).to have_text(@item_beamer.name)
+    expect(page).not_to have_text(@item_whiteboard.name)
+  end
+
+  it "shows all available categories" do
+    click_button("filter")
+
+    expect(page).to have_text(@item_book.category)
+    expect(page).to have_text(@item_beamer.category)
+    expect(page).to have_text(@item_whiteboard.category)
   end
 
   it "shows available items first" do
