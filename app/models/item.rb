f312b80934152d0594495e2dee7bba429baf6e4b
Pierre Burghardt <pierre.burghardt@student.hpi.de>
2023-01-25 15:17:15 +0100

    make rspec work, but remove holder asociation

--- app/models/item.rb
+++ app/models/item.rb
@@ -29,7 +29,7 @@ class Item < ApplicationRecord
   has_one :owning_user, through: :ownership_permission, source: :user_or_group, source_type: 'User'
   has_one :owning_group, through: :ownership_permission, source: :user_or_group, source_type: 'Group'
 
-  belongs_to :holder, class_name: 'User', foreign_key: "holder", inverse_of: :lent_items, optional: true
+  # belongs_to :holder, class_name: 'User', foreign_key: "holder", inverse_of: :lent_items, optional: true
 
   validates :name, presence: true
   validates :category, presence: true
@@ -174,7 +174,7 @@ class Item < ApplicationRecord
   end
 
   def print_rental_duration
-    seconds = rental_duration_sec
+    seconds = rental_duration_sec ? rental_duration_sec : 0
     if seconds < 7 * 86_400
       print_rental_duration_days(seconds)
     elsif seconds < 4 * (7 * 86_400)
